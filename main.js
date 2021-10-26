35bf7e79704226698e12800c5f360b0c09cdf449
Yu4iy <yurimohyliuk@gmail.com>
2021-10-26 15:09:32 +0200

    add alpha 'dadi'

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('test')
+
+
+// Generare un numero random da 1 a 6
+let userRand = Math.floor(Math.random() * 6) + 1
+console.log(userRand)
+let computerRand = Math.floor(Math.random() * 6) + 1
+console.log(computerRand)
+
+// Stabilire il vincitore
+if (userRand > computerRand){
+	console.log(`Numero ${userRand} Hai Vinto`)
+}else if(userRand < computerRand) {
+	console.log(`Numero ${computerRand} Hai perso`)
+}else if(userRand = computerRand) {
+	console.log('pari')
+}
